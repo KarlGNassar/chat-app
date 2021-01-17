1ba761e63500f69dd08082b1fba3a685f3c01fe0
KARL NASSAR <kgnassar01@gmail.com>
2021-01-17 15:40:34 +0200

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './App.css';
+import Chat from './Chat';
+import Sidebar from './Sidebar';
 
 function App() {
   return (
     <div className="app">
-      
+      {/* Login Page */}
+      <div className="app__body">
+        <Sidebar />
+        <Chat />
+      </div>
     </div>
   );
 }
