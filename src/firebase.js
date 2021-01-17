783a5db5325fcdad9bbfe21330e689f9db619d6b
KARL NASSAR <kgnassar01@gmail.com>
2021-01-17 14:36:32 +0200

    init

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCWoVsUYehCiz1m82SoelusDPVuasyabk4",
+    authDomain: "chat-app-4405a.firebaseapp.com",
+    projectId: "chat-app-4405a",
+    storageBucket: "chat-app-4405a.appspot.com",
+    messagingSenderId: "990337722343",
+    appId: "1:990337722343:web:cf76541c622d5a1097ff42",
+    measurementId: "G-ZQ23CXCZTN"
+};
