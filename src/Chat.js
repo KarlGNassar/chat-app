005e74c510ce5091c425ba9dc5803f95c71fd84a
KARL NASSAR <kgnassar01@gmail.com>
2021-01-17 17:31:07 +0200

    chat

--- src/Chat.js
+++ src/Chat.js
@@ -1,10 +1,78 @@
 import React from 'react'
 import './Chat.css'
+import { Avatar, IconButton } from '@material-ui/core'
+import SearchOutlined from '@material-ui/icons/SearchOutlined'
+import AttachFileIcon from '@material-ui/icons/AttachFile'
+import MoreVertIcon from '@material-ui/icons/MoreVert'
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon'
+import SendIcon from '@material-ui/icons/Send'
 
 function Chat() {
     return (
         <div className="chat">
-            <h1>I am the Chat</h1>
+            <div className="chat__header">
+                <Avatar />
+
+                <div className="chat__headerInfo">
+                    <h3>Room name</h3>
+                    <p>Last seen at...</p>
+                </div>
+
+                <div className="chat__headerRight">
+                    <IconButton>
+                        <SearchOutlined />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFileIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+
+            <div className="chat__body">
+                <p className="chat__message">
+                    <span className="chat__name">
+                        Karl
+                    </span>
+                    This is a message
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+
+                <p className="chat__message chat__reciever">
+                    <span className="chat__name">
+                        Karl
+                    </span>
+                    This is a message
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+
+                <p className="chat__message">
+                    <span className="chat__name">
+                        Karl
+                    </span>
+                    This is a message
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+            </div>
+
+            <div className="chat__footer">
+                <InsertEmoticonIcon />
+                <form>
+                    <input placeholder="Type a message" type="text" />
+                    <button type="submit">
+                        Send a message
+                    </button>
+                </form>
+                <SendIcon />
+            </div>
         </div>
     )
 }
