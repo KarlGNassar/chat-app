f2acca8858f080147848588414c4a026e58f8135
KARL NASSAR <kgnassar01@gmail.com>
2021-01-17 20:51:45 +0200

    final touches

--- src/Chat.js
+++ src/Chat.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Chat.css'
 import { Avatar, IconButton } from '@material-ui/core'
 import SearchOutlined from '@material-ui/icons/SearchOutlined'
@@ -6,8 +6,23 @@ import AttachFileIcon from '@material-ui/icons/AttachFile'
 import MoreVertIcon from '@material-ui/icons/MoreVert'
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon'
 import SendIcon from '@material-ui/icons/Send'
+import axios from './axios'
 
 function Chat({ messages }) {
+    const [input, setInput] = useState('')
+    const sendMessage = async (e) => {
+        e.preventDefault()
+
+        await axios.post('/api/v1/messages/new', {
+            message: input,
+            name: 'Karl Nassar',
+            timestamp: 'Just Now',
+            received: true,
+        })
+
+        setInput('')
+    }
+
     return (
         <div className="chat">
             <div className="chat__header">
@@ -34,7 +49,7 @@ function Chat({ messages }) {
             <div className="chat__body">
                 {
                     messages.map((message, i) => (
-                        <p key={i} className={`chat__message ${message.received === true ? 'chat__receiver' : ''}`}>
+                        <p key={i} className={`chat__message ${message.received === true ? 'chat__receiver' : ' '}`}>
                             <span className="chat__name">
                                 {message.name}
                             </span>
@@ -53,8 +68,8 @@ function Chat({ messages }) {
             <div className="chat__footer">
                 <InsertEmoticonIcon />
                 <form>
-                    <input placeholder="Type a message" type="text" />
-                    <button type="submit">
+                    <input value={input} onChange={e => setInput(e.target.value)}placeholder="Type a message" type="text" />
+                    <button onClick={sendMessage} type="submit">
                         Send a message
                     </button>
                 </form>
